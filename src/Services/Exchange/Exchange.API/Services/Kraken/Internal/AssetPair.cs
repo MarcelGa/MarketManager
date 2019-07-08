514cdb5ed9ea6d7fc276155185dacce337d918a8
Marcel <galovic.marcel@gmail.com>
2019-07-08 13:54:36 +0200

    Update AssetPair.cs

--- src/Services/Exchange/Exchange.API/Services/Kraken/Internal/AssetPair.cs
+++ src/Services/Exchange/Exchange.API/Services/Kraken/Internal/AssetPair.cs
@@ -7,5 +7,17 @@ namespace Exchange.API.Services.Kraken.Internal
 {
     internal class AssetPair
     {
+        /// <summary>
+        /// The name of the asset pair
+        /// </summary>
+        public String PairName { get; set; }
+        /// <summary>
+        /// The name of the base currency for the asset pair
+        /// </summary>
+        public String BaseName { get; set; }
+        /// <summary>
+        /// The name of the quote currency for the asset pair
+        /// </summary>
+        public String QuoteName { get; set; }
     }
 }
